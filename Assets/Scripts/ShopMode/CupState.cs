c10762068b4d149ee880ab40c03aa5ed054ee0a6
Brooklyn Harden <bharden@udel.edu>
2025-05-02 22:12:46 -0400

    The matcha drink works, the user can add boba by clicking the boba pearls and then click the matcha carton to add the drink. However if the user does not want theboba but only the matcha the user is able to just add the matcha, I didnt get the cup to seal just yet but after I finish doing this to all the drinks I will move on to that

---
+++ Assets/Scripts/ShopMode/CupState.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CupState : MonoBehaviour
+{
+    public bool hasBoba = false;
+    public bool hasMatcha = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
