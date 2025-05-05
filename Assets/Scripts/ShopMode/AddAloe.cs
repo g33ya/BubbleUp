341d19c4dcf40fa43f77bee657b6b4f45b3231e8
Brooklyn Harden <bharden@udel.edu>
2025-05-04 21:11:53 -0400

    the matcha works for every cup

--- Assets/Scripts/ShopMode/AddAloe.cs
+++ Assets/Scripts/ShopMode/AddAloe.cs
@@ -3,15 +3,28 @@ using UnityEngine;
 public class AddAloe : MonoBehaviour
 {
       public Sprite aloeCupSprite;
+      public Sprite matchaWithAloeSprite;
     // public GameObject cupObject;
 
     void OnMouseDown()
     {
-        if(CupButtonSpawner.currentCup != null && aloeCupSprite != null){
+        
+        GameObject cup = CupButtonSpawner.currentCup;
+        if(cup == null){
+            return;
+        }
+        
+        if(cup != null && aloeCupSprite != null){
 
-            SpriteRenderer sr = CupButtonSpawner.currentCup.GetComponent<SpriteRenderer>();
-            if (sr != null){
-                sr.sprite = aloeCupSprite;
+            SpriteRenderer sr = cup.GetComponent<SpriteRenderer>();
+            CupState state = cup.GetComponent<CupState>();
+            if (sr != null && state != null){
+                if(state.hasMatcha){
+                    sr.sprite = matchaWithAloeSprite;
+                }else{
+                     sr.sprite = aloeCupSprite;
+                }
+               state.hasAloe = true;
             }
         }
     }
