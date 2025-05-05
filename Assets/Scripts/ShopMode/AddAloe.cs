147d9dea0f62e0025583cfdc24088685380def7c
brooklynharden <159091522+brooklynharden@users.noreply.github.com>
2025-05-05 14:04:16 -0400

    Revert "the matcha works for every cup"

--- Assets/Scripts/ShopMode/AddAloe.cs
+++ Assets/Scripts/ShopMode/AddAloe.cs
@@ -3,28 +3,15 @@ using UnityEngine;
 public class AddAloe : MonoBehaviour
 {
       public Sprite aloeCupSprite;
-      public Sprite matchaWithAloeSprite;
     // public GameObject cupObject;
 
     void OnMouseDown()
     {
-        
-        GameObject cup = CupButtonSpawner.currentCup;
-        if(cup == null){
-            return;
-        }
-        
-        if(cup != null && aloeCupSprite != null){
+        if(CupButtonSpawner.currentCup != null && aloeCupSprite != null){
 
-            SpriteRenderer sr = cup.GetComponent<SpriteRenderer>();
-            CupState state = cup.GetComponent<CupState>();
-            if (sr != null && state != null){
-                if(state.hasMatcha){
-                    sr.sprite = matchaWithAloeSprite;
-                }else{
-                     sr.sprite = aloeCupSprite;
-                }
-               state.hasAloe = true;
+            SpriteRenderer sr = CupButtonSpawner.currentCup.GetComponent<SpriteRenderer>();
+            if (sr != null){
+                sr.sprite = aloeCupSprite;
             }
         }
     }
