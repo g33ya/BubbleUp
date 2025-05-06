39dbfd44d6ada33207dd8223709a761f4459336a
g33ya <115582758+g33ya@users.noreply.github.com>
2025-05-06 00:15:14 -0400

    Revert "Merge pull request #100 from g33ya/HomeMode-VikiNiki123"
    This reverts commit b122bc8e43d39005a70baf1c664c00469b1c10d9, reversing
    changes made to 7a78d0725afa111e0c29eada5a2875721f1aee18.

--- Assets/Scripts/HomeMode/OpenBed.cs
+++ Assets/Scripts/HomeMode/OpenBed.cs
@@ -5,21 +5,10 @@ public class OpenBed : MonoBehaviour
     public GameObject calendarUI;
     public GameObject computerUI;
     public GameObject bedUI;
-    public GameObject doorUI;
-    public GameObject catUI;
-    public GameObject recordUI;
-    public GameObject journalUI;
-    public GameObject booksUI;
 
     void OnMouseDown()
     {
-        if (!computerUI.activeSelf && 
-            !calendarUI.activeSelf && 
-            !doorUI.activeSelf &&
-            !catUI.activeSelf &&
-            !recordUI.activeSelf &&
-            !journalUI.activeSelf &&
-            !booksUI.activeSelf)
+        if (!computerUI.activeSelf && !calendarUI.activeSelf)
         {
             bedUI.SetActive(true);
         }
