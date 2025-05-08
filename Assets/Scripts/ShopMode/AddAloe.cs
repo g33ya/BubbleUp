b95676d89e6679309f6457e08b9d3cf55d1f68a1
g33ya <115582758+g33ya@users.noreply.github.com>
2025-05-08 17:58:44 -0400

    Revert "Merge pull request #102 from g33ya/brooklynharden"
    This reverts commit 0bd7c9def09e3ac08bb32bc75b23c84cab2e5582, reversing
    changes made to 39dbfd44d6ada33207dd8223709a761f4459336a.

--- Assets/Scripts/ShopMode/AddAloe.cs
+++ Assets/Scripts/ShopMode/AddAloe.cs
@@ -3,34 +3,15 @@ using UnityEngine;
 public class AddAloe : MonoBehaviour
 {
       public Sprite aloeCupSprite;
-      public Sprite matchaWithAloeSprite;
-
-      public Sprite mangoWithAloeSprite;
-      public Sprite taroWithAloeSprite;
-      public Sprite milkWithAloeSprite;
     // public GameObject cupObject;
 
     void OnMouseDown()
     {
         if(CupButtonSpawner.currentCup != null && aloeCupSprite != null){
-            GameObject cup = CupButtonSpawner.currentCup;
-        SpriteRenderer sr = cup.GetComponent<SpriteRenderer>();
-        CupState state = cup.GetComponent<CupState>();
-            if (sr != null && state != null){
-                if(state.hasMatcha){
-                    sr.sprite = matchaWithAloeSprite;
-                }else if(state.hasMango){
-                    sr.sprite = mangoWithAloeSprite;
-                }else if(state.hasTaro){
-                    sr.sprite = mangoWithAloeSprite;
-                }
-                else if(state.hasMilk){
-                    sr.sprite = milkWithAloeSprite;
-                }
-                else{
-                     sr.sprite = aloeCupSprite;
-                }
-               state.hasAloe = true;
+
+            SpriteRenderer sr = CupButtonSpawner.currentCup.GetComponent<SpriteRenderer>();
+            if (sr != null){
+                sr.sprite = aloeCupSprite;
             }
         }
     }
