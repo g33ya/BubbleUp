0b6d03a7520745329fb515b1727395fded22d054
Victoria Nikolaeva <150319175+victoria-nikolaeva04@users.noreply.github.com>
2025-05-19 22:54:03 -0400

    Merge the new items from main & created new outline script

---
+++ Assets/Scripts/HomeMode/PermanentOutline.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PermanentOutline : MonoBehaviour
+{
+    public Color outlineColor = Color.yellow;
+    public float scaleFactor = 1.05f;
+
+    private GameObject outlineObj;
+
+    void Start()
+    {
+        // Duplicate the original sprite
+        outlineObj = new GameObject("Outline");
+        outlineObj.transform.parent = transform;
+        outlineObj.transform.localPosition = Vector3.zero;
+        outlineObj.transform.localScale = Vector3.one * scaleFactor;
+
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        SpriteRenderer outlineSR = outlineObj.AddComponent<SpriteRenderer>();
+
+        outlineSR.sprite = sr.sprite;
+        outlineSR.sortingLayerID = sr.sortingLayerID;
+        outlineSR.sortingOrder = sr.sortingOrder - 1;
+        outlineSR.color = outlineColor;
+    }
+}
+
